ac2c7c84a536fa20c3f26ce8370e8ed6ba7fcb01
Princgrewal <164895925+Princgrewal@users.noreply.github.com>
2025-01-28 20:17:44 -0700

    week2 commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,8 @@
+export const StudentInfo = () => {
+    return (
+        <main>
+            <h1>Princedeep Singh</h1>
+            <p><a href="https://github.com/Princgrewal/cprg306-assignments">github</a></p>
+        </main>
+    );
+}
